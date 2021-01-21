a931de2b53026dad17724a311f249d0e2eb2bef0
5piffe <spiffe84@gmail.com>
2021-01-21 13:23:04 +0100

    CityGenerator pogress
    Lite stuff + addade min test-scene

--- Assets/Scripts/CityGeneratorTest.cs
+++ Assets/Scripts/CityGeneratorTest.cs
@@ -5,23 +5,79 @@ using UnityEngine;
 
 public class CityGeneratorTest : MonoBehaviour
 {
-	[SerializeField] private int sizeX = 20, sizeY = 20;
-	[SerializeField] private float houseSpacing = 1.5f;
+	[Header("CITYGENERATION")]
+	[SerializeField] private int citySizeX = 25;
+	[SerializeField] private int citySizeZ = 12;
+	[SerializeField] private float minXOffset = 0f;
+	[SerializeField] private float maxXOffset = 1.5f;
+	[SerializeField] private float minZOffset = 2.5f;
+	[SerializeField] private float maxZOffset = 4f;
+	[SerializeField, Range(0,1)] private float houseCreationChance = 0.65f;
+	[Header("")]
+	[SerializeField] private float houseSpacing = 8.0f;
+	[SerializeField] private float minHouseThickness = 1.5f;
+	[SerializeField] private float maxHouseThickness = 6f;
+	[SerializeField] private float minHouseHeight = 8f;
+	[SerializeField] private float maxHouseHeight = 28f;
+
+	private float randomThickness, randomHeight;
 
 	private void Start()
 	{
-		CubeGrid(sizeX, sizeY);
+		//CubeGrid(sizeX, sizeY);
 	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			foreach (Transform child in transform)
+			{
+				Destroy(child.gameObject);
+			}
+
+			CubeGrid(citySizeX, citySizeZ);
+		}
+	}
+
 	private void CubeGrid(int sizeX, int sizeY)
 	{
 		for (int x = 0; x < sizeX; x++)
 		{
-			// fixa spacing
 			for (int y = 0; y < sizeY; y++)
 			{
-				GameObject house = GameObject.CreatePrimitive(PrimitiveType.Cube);
-				
+				var spacingX = x * houseSpacing;
+				var spacingY = y * houseSpacing;
+
+				if (ChanceOfCreatingHouse(houseCreationChance))
+				{
+					randomThickness = UnityEngine.Random.Range(minHouseThickness, maxHouseThickness);
+					randomHeight = UnityEngine.Random.Range(minHouseHeight, maxHouseHeight);
+
+					GameObject house = GameObject.CreatePrimitive(PrimitiveType.Cube);
+					house.transform.SetParent(transform);
+
+					house.transform.localScale = new Vector3(randomThickness, randomHeight, randomThickness);
+
+					house.transform.localPosition = new Vector3(spacingX, transform.position.y + randomHeight * 0.5f, spacingY)
+						+ RandomOffset(minXOffset,maxXOffset,minZOffset,maxZOffset);
+				}
 			}
 		}
+	}
+
+	private bool ChanceOfCreatingHouse(float chance)
+	{
+		float rnd = UnityEngine.Random.Range(0.0f, 1.0f);
+		
+		return (rnd <= chance) ? true : false;
+	}
+
+	private Vector3 RandomOffset(float minXOffset, float maxXOffset, float minZOffset, float maxZOffset)
+	{
+		float x = UnityEngine.Random.Range(minXOffset, maxXOffset);
+		float z = UnityEngine.Random.Range(minZOffset, maxZOffset);
+
+		return new Vector3(x, 0f, z);
 	}
 }
